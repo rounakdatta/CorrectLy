a335ec89586dba20459dfaa96177c1b1f7d7e435
rounakdatta <rounakdatta12@gmail.com>
2018-06-28 15:51:42 +0530

    table of corrections

--- src/checker.py
+++ src/checker.py
@@ -2,12 +2,47 @@ import language_check as lc
 import spacy
 from nltk import Tree
 from pattern.en import conjugate, lemma, lexeme, INFINITIVE, PRESENT, PAST, PARTICIPLE, FUTURE, SG, PL, INDICATIVE, IMPERATIVE, CONDITIONAL, SUBJUNCTIVE, PROGRESSIVE
-import numpy as np # only for writing, reading the master_dictionary
+import numpy as np
+import os
+from sympound import sympound
+import platform
+
+# all the tools required for sentence structure correction
+distancefun = None
+if platform.system() != "Windows":
+	from pyxdameraulevenshtein import damerau_levenshtein_distance
+	distancefun = damerau_levenshtein_distance
+else:
+	from jellyfish import levenshtein_distance
+	distancefun = levenshtein_distance
+ssc = sympound(distancefun=distancefun, maxDictionaryEditDistance=3)
 
 tool = lc.LanguageTool('en-US')
 
 en_nlp = spacy.load('en')
 combos = []
+
+def encode_sentence(english):
+	doc = en_nlp(english)
+	deps_dict = np.load('./research/deps_dict.npy').item()
+
+	sent_struct = []
+	for token in doc:
+		try:
+			sent_struct.append(deps_dict[token.dep_.lower()])
+		except:
+			print('no key!')
+
+	sentence_code = ''.join(sent_struct)
+	return sentence_code
+
+def decode_coding(code):
+	inv_deps_dict = np.load('./research/inv_deps_dict.npy').item()
+	code_list = list(code.replace(' ', ''))
+	decoded_list = []
+	for char in code_list:
+		decoded_list.append(inv_deps_dict[char])
+	return decoded_list
 
 def VB_VB_VB_correction(payload, raw_text, error_count): # correct errors of type has-been-walking
 	if(payload.tag_[:2] != 'VB' and payload.tag_[:2] != 'NN'  and payload.tag_[:2] != 'JJ'):
@@ -114,13 +149,52 @@ def VB_IN_NN_correction(payload, raw_text, master_dictionary, error_count): # co
 						return raw_text, error_count
 	return raw_text, error_count
 
+def sentence_correction(payload, sc_dict):
+	# optional, only if adding dictionary items is required
+	# ssc.create_dictionary_entry("bonjour", 1)
+	
+	ssc.load_dictionary("./research/sentence_codes.txt", term_index=0, count_index=1)
+	payload = encode_sentence(payload)
+	print(payload)
+	
+	if payload in sc_dict:
+		print('correct!')
+		return 'CORRECT', 'CORRECT'
+	
+	try:
+		# result = ssc.lookup_compound(input_string=payload, edit_distance_max=1) # choose edit_distance carefully
+		result = ssc.lookup(input_string=payload, verbosity=0, edit_distance_max=1) # choose edit_distance carefully
+	except TypeError:
+		print('no matches!')
+		return 'NOTFOUND', 'NOTFOUND'
+	
+	if(result == []):
+		return 'NOTFOUND', 'NOTFOUND'
+	
+	print(result[-1])
+	result = str(result[-1]).split(':')
+	print('wrong :      ' + payload)
+	print('correction : ' + result[0])
+	# print('position : ' + result[2])
+	
+	# ssc.save_pickle("symspell.pickle")
+	# ssc.load_pickle("symspell.pickle")
+
+	wrong_sentence_structure = decode_coding(payload)
+	correct_sentence_structure = decode_coding(''.join(result[0]))
+
+	return '-'.join(wrong_sentence_structure), '-'.join(correct_sentence_structure)
+
 def modify(text):
 
 	correctly = np.load('./research/correctly.npy').item()
+	sentence_codes_dict = np.load('./research/sentence_correction_dict.npy').item()
 
 	vv_errors = 0
 	vvv_errors = 0
 	prep_errors = 0
+	wrong_text_structure = []
+	correct_text_structure = []
 
 	matches = tool.check(text)
 	text = lc.correct(text,matches)
@@ -132,9 +206,12 @@ def modify(text):
 		text, e1 = VB_VB_correction(sent.root, text, 0)
 		text, e2 = VB_VB_VB_correction(sent.root, text, 0)
 		text, e3 = VB_IN_NN_correction(sent.root, text, correctly, 0)
+		w, c = sentence_correction(sent.text, sentence_codes_dict)
+		wrong_text_structure.append(w)
+		correct_text_structure.append(c)
 		vv_errors += e1
 		vvv_errors += e2
 		prep_errors += e3
 
-	errors = [["Spelling errors", len(matches)], ["V-V errors", vv_errors], ["V-V-V errors", vvv_errors], ["Preposition errors", prep_errors], ["Total errors", (len(matches) + vv_errors + vvv_errors + prep_errors)]]
+	errors = [["Spelling errors", len(matches)], ["V-V errors", vv_errors], ["V-V-V errors", vvv_errors], ["Preposition errors", prep_errors], ["Total errors", (len(matches) + vv_errors + vvv_errors + prep_errors)], wrong_text_structure, correct_text_structure]
 	return text, errors
